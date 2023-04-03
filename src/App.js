bbd331243800d81622380661e635b3ecfee5bb2d
Kimuchan0312 <75709261+Kimuchan0312@users.noreply.github.com>
2023-04-03 11:02:23 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+const api = {
+  key: "97386b34b7ec6cb71b16bc3a445548af",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
+  const [searchInput, setSearchInput] = useState("");
+  const [searchCity, setSearchCity] = useState("");
+  const [weatherInfo, setWeatherInfo] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    const fetchweatherData = async () => {
+      if (!searchCity) return;
+      setLoading(true);
+      // Process
+      try {
+        const url = `${api.base}weather?q=${searchCity}&units=metric&APPID=${api.key}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        if (response.ok) {
+          setWeatherInfo({
+            name: data.name,
+            country: data.sys.country,
+            description: data.weather[0].description,
+            temp: data.main.temp,
+          });
+          setErrorMessage("");
+        } else {
+          setErrorMessage(data.message);
+        }
+      } catch (error) {
+        setErrorMessage(error.message);
+      }
+      setLoading(false);
+    };
+    fetchweatherData();
+  }, [searchCity]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSearchCity(searchInput);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+      <h1 className="title">Weather App</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="input"
+          type="text"
+          placeholder="City"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+        <button className="btn" type="submit">Search</button>
+      </form>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          {errorMessage ? (
+            <div className="error-message" style={{ color: "red" }}>
+              {errorMessage}
+            </div>
+          ) : (
+            <div className="weather-info">
+              {weatherInfo && (
+                <div>
+                  <h2>{weatherInfo.name}</h2>
+                  <p>{weatherInfo.country}</p>
+                  <p>{weatherInfo.description}</p>
+                  <p>{weatherInfo.temp}°C</p>
+                </div>
+              )}
+            </div>
+          )}
+        </>
+      )}
+      </div>
+    </>
   );
 }
 
